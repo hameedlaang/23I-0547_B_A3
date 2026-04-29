1551963912199621084bdb2976c6caa852c987cc
hameedlaang <i230547@isb.nu.edu.pk>
2026-04-29 16:20:43 +0500

    fixed layout and deployment issues

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,16 @@
+// app/layout.js
+
 import "./globals.css";
+
+export const metadata = {
+  title: "Property Dealer CRM",
+  description: "CRM System using Next.js + MongoDB"
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html>
-      <body>{children}</body>
+    <html lang="en">
+      <body className="bg-gray-100">{children}</body>
     </html>
   );
 }
